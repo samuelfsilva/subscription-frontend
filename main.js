cee628793c2c1c41f45290205ad30ea680717cf3
samuelfsilva <samukfer@gmail.com>
2021-01-27 12:58:05 -0300

    Refatoração para referências de servidor

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', '..', 'www')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', 'www', 'index.html'));
+});
+
+app.get('/subscription', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', 'www', 'index.html'));
+});
+  
+app.listen(21085);
