52c0236172cd71884cd9635f696970d927622403
samuelfsilva <samukfer@gmail.com>
2021-01-29 18:56:05 -0300

    Refatoração.

--- main.js
+++ main.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 const app = express();
 
+const port = 21085;
+
 app.use(express.static(path.join(__dirname, '..', '..', 'www')));
 
-app.get('/', function (req, res) {
+const redirectionHttps = function (req, res, next) {
+  if (req.get('X-Forwarded-Proto') === 'http') {
+    const redirectTo = `https:\/\/${req.hostname}${req.url}`;
+    res.redirect(301, redirectTo);
+  } else {
+    next();
+  }
+};
+
+app.get('/*', redirectionHttps, function (req, res) {
   res.sendFile(path.join(__dirname, '..', '..', 'www', 'index.html'));
 });
 
-app.get('/subscription', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', '..', 'www', 'index.html'));
-});
-  
-app.listen(21085);
+app.listen(port);
