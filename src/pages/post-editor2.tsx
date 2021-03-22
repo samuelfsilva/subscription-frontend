f2003bfa9f85586ef5f7e2e4b9392a1c3445d0e6
samuelfsilva <samukfer@gmail.com>
2021-03-22 00:51:50 -0300

    Melhorias no editor WYSIWYG

--- src/pages/post-editor2.tsx
+++ src/pages/post-editor2.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
-import { Jodit } from "jodit"
+import React, { useState, useRef } from 'react'
+import { Jodit, IJodit } from 'jodit'
+import JoditEditor from 'jodit-react'
 
 import Button from '../components/Button'
 
@@ -7,20 +8,48 @@ import '../styles/pages/post-editor.css'
 
 const PostEditor = () => {
 
-  useEffect(()  => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    var editor = new Jodit('#editor')
-  })
+  const editor = useRef(null)
+	const [content, setContent] = useState('')
+	
+	const config: IJodit['options'] = {
+    ...Jodit.defaultOptions,
+		readonly: false,
+    uploader: {
+      ...Jodit.defaultOptions.uploader,
+      insertImageAsBase64URI: false,
+      url: "https://xdsoft.net/jodit/connector/index.php?action=fileUpload"
+    }
+	}
+
+  const onBlur = (event: any) => { const editorValue = event.target.innerHTML; setContent(editorValue); };
+
+  // useEffect(()  => {
+  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //   var editor = new Jodit('#editor')
+  // })
   
   return (
       <div className="editor-container">
-          <label htmlFor="title" id="lbtitle">Título</label>
+          {/* <label htmlFor="title" id="lbtitle">Título</label>
           <input type="text" id="title" />
           <textarea id="editor" name="editor" placeholder="Escreva aqui..."></textarea>
           <div className="controlls">
             <Button name="cancel" label="Cancelar" className="cancel-button" />
             <Button name="enviar" label="Enviar" type="submit" className="send-button" />
-          </div>
+          </div> */}
+        <label htmlFor="title" id="lbtitle">Título</label>
+        <input type="text" id="title" />
+        <JoditEditor
+          ref={editor}
+          value={content}
+          config={config}
+		      tabIndex={1} // tabIndex of textarea
+		      onBlur={onBlur}
+          onChange={newContent => {}}/>
+        <div className="controlls">
+          <Button name="cancel" label="Cancelar" className="cancel-button" />
+          <Button name="enviar" label="Enviar" type="submit" className="send-button" />
+        </div>
       </div>
   );
     
