83c909f875022d05a22aa85debcb6cec0119534b
samuelfsilva <samukfer@gmail.com>
2021-04-01 11:51:53 -0300

    Melhorias de interface.

--- src/pages/post-editor2.tsx
+++ src/pages/post-editor2.tsx
@@ -17,26 +17,17 @@ const PostEditor = () => {
     uploader: {
       ...Jodit.defaultOptions.uploader,
       insertImageAsBase64URI: false,
-      url: "https://xdsoft.net/jodit/connector/index.php?action=fileUpload"
+      url: "https://xdsoft.net/jodit/connector/index.php?action=fileUpload",
     }
 	}
 
-  const onBlur = (event: any) => { const editorValue = event.target.innerHTML; setContent(editorValue); };
-
-  // useEffect(()  => {
-  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  //   var editor = new Jodit('#editor')
-  // })
+  const onBlur = (event: any) => { 
+    const editorValue = event.target.innerHTML;
+    setContent(editorValue);
+  };
   
   return (
       <div className="editor-container">
-          {/* <label htmlFor="title" id="lbtitle">Título</label>
-          <input type="text" id="title" />
-          <textarea id="editor" name="editor" placeholder="Escreva aqui..."></textarea>
-          <div className="controlls">
-            <Button name="cancel" label="Cancelar" className="cancel-button" />
-            <Button name="enviar" label="Enviar" type="submit" className="send-button" />
-          </div> */}
         <label htmlFor="title" id="lbtitle">Título</label>
         <input type="text" id="title" />
         <JoditEditor
